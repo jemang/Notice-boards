3392971b7553f01ba3ac4e1e627802c18cec1ce3
jemang <jempalo94@gmail.com>
2024-04-03 06:15:28 +0000

    scaffold events

---
+++ db/migrate/20240323145712_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.string :title, null: false
+      t.text :note
+      t.string :type
+      t.string :category
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+end
